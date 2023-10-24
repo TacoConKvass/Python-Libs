5c194a98f5090334d41bd1e12c545615d063a87e
TacoConKvass <128845692+TacoConKvass@users.noreply.github.com>
2023-10-24 15:43:36 +0200

    [Addition] - Added 'Line' shape type

--- GeometryLib.py
+++ GeometryLib.py
@@ -6,10 +6,10 @@ class Rectangle:
 		self.center = center
 		self.width = width
 		self.height = height
-		self.vertices = [  Vector2(width/2, height/2),
-				Vector2(width/2, -height/2),
-				Vector2(-width/2, -height/2),
-				Vector2(-width/2, height/2)  ]
+		self.vertices = [ Vector2(width/2, height/2),
+						Vector2(width/2, -height/2),
+						Vector2(-width/2, -height/2),
+						Vector2(-width/2, height/2) ]
 									
 	def GetVertices(self) -> list:
 		return [vector.show() for vector in self.vertices]
@@ -19,5 +19,22 @@ class Rectangle:
 		rotatedRect.vertices = [vector.RotatedBy(angleInRadians) for vector in self.vertices]
 		return rotatedRect
 	
+	def __str__(self) -> str:
+		return str(self.GetVertices())
+
+class Line:
+	def __init__(self, length: float, center: Vector2 = Vector2(0, 0)) -> None:
+		self.center = center
+		self.length = length
+		self.vertices = [ Vector2(0, length / 2), Vector2(0, -length / 2)]
+	
+	def GetVertices(self) -> list:
+		return [vector.show() for vector in self.vertices]
+
+	def RotatedBy(self, angleInRadians: float) -> 'Line':
+		rotatedLine = Line(self.length)
+		rotatedLine.vertices = [vector.RotatedBy(angleInRadians) for vector in self.vertices]
+		return rotatedLine
+
 	def __str__(self) -> str:
 		return str(self.GetVertices())
