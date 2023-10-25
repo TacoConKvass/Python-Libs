50d5f44d24e301cef65b42a5fbf778cceb06ae0f
TacoConKvass <128845692+TacoConKvass@users.noreply.github.com>
2023-10-25 21:27:22 +0200

    [Addition] - Polygon class for GeometryLib

--- GeometryLib.py
+++ GeometryLib.py
@@ -54,7 +54,7 @@ class Circle:
 		self.vertices = self.GenerateVertices()
 
 	def GenerateVertices(self) -> list:
-		return [Vector2(self.radius, 0).RotatedBy(rad(x * 9)) for x in range(0, 40)]
+		return [Vector2(self.radius, 0).RotatedBy(rad(x * 12)) for x in range(0, 60)]
 
 	def GetVertices(self) -> list:
 		return [vector.show() for vector in self.vertices]
@@ -90,6 +90,28 @@ class Triangle:
 
 	def GetArea(self):
 		return self.side * self.height / 2
+
+	def __str__(self) -> str:
+		return str(self.GetVertices())
+		
+class Polygon:
+	def __init__(self, vertices: list = None, center: Vector2 = Vector2(0, 0)) -> None:
+		self.center = center
+		self.vertices = vertices if vertices != None else self.GenerateVertices()
+		
+	def GenerateVertices(self) -> list:
+		return [Vector2(.5, 0),
+					Vector2(0, -.8), 
+					Vector2(-.3, -.1), 
+					Vector2(0, .5)]
+	
+	def GetVertices(self) -> list:
+		return [vector.show() for vector in self.vertices]
+
+	def RotatedBy(self, angleInRadians: float) -> 'Triangle':
+		rotatedPolygon = Polygon(self.vertices)
+		rotatedPolygon.vertices = [vector.RotatedBy(angleInRadians) for vector in self.vertices]
+		return rotatedPolygon
 
 	def __str__(self) -> str:
 		return str(self.GetVertices())
